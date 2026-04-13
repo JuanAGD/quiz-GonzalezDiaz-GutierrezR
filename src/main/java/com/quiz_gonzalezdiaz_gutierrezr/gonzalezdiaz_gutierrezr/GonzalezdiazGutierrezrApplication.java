92557f27983b504ae83b7b32611d53772bb5033a
JuanAGD <gonzalezdiaz057@gmail.com>
2026-04-13 12:17:40 -0500

    initial commit

---
+++ src/main/java/com/quiz_gonzalezdiaz_gutierrezr/gonzalezdiaz_gutierrezr/GonzalezdiazGutierrezrApplication.java
@@ -0,0 +1,13 @@
+package com.quiz_gonzalezdiaz_gutierrezr.gonzalezdiaz_gutierrezr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GonzalezdiazGutierrezrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GonzalezdiazGutierrezrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quiz_gonzalezdiaz_gutierrezr/gonzalezdiaz_gutierrezr/GonzalezdiazGutierrezrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quiz_gonzalezdiaz_gutierrezr.gonzalezdiaz_gutierrezr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GonzalezdiazGutierrezrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
