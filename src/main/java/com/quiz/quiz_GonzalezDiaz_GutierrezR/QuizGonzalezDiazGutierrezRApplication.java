752b1ed27e4a663e454e3d9b993919212713b550
JuanAGD <gonzalezdiaz057@gmail.com>
2026-04-13 12:25:13 -0500

    Add security

---
+++ src/main/java/com/quiz/quiz_GonzalezDiaz_GutierrezR/QuizGonzalezDiazGutierrezRApplication.java
@@ -0,0 +1,13 @@
+package com.quiz.quiz_GonzalezDiaz_GutierrezR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizGonzalezDiazGutierrezRApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizGonzalezDiazGutierrezRApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quiz/quiz_GonzalezDiaz_GutierrezR/QuizGonzalezDiazGutierrezRApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quiz.quiz_GonzalezDiaz_GutierrezR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizGonzalezDiazGutierrezRApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
